4af85126a476c4a3b6a53c670157c8f489509098
Mattia Righetti <matt95.righetti@gmail.com>
2021-01-29 10:49:42 +0100

    Fixed nan value in stats view

--- Lode/ViewModels/StatsViewViewModel.swift
+++ Lode/ViewModels/StatsViewViewModel.swift
@@ -11,14 +11,18 @@ class StatsViewViewModel: ObservableObject {
             totalCourses = Double(newValue.count)
             totalCfu = newValue.compactMap { course in Double(course.cfu) }.reduce(0, { $0 + $1 })
             let markSum = newValue.compactMap { Double($0.expectedMark) * Double($0.cfu) }.reduce(0, { $0 + $1 })
-            expectedAverage = markSum / Double(totalCfu)
-            projectedGraduationGrade = expectedAverage * 110.0 / 30.0
+            if totalCfu != 0 {
+                expectedAverage = markSum / Double(totalCfu)
+                projectedGraduationGrade = expectedAverage * 110.0 / 30.0
+            }
             passedExams = newValue.filter { $0.mark != 0 }
             numPassedExams = Double(passedExams.count)
             gainedCfu = passedExams.compactMap { Double($0.cfu) }.reduce(0, { $0 + $1 })
             let average = passedExams.compactMap { Double($0.cfu) * Double($0.mark) }.reduce(0, { $0 + $1 })
-            currentAverage = average / gainedCfu
-            currentProjectedGraduationGrade = currentAverage * 110.0 / 30.0
+            if gainedCfu != 0 {
+                currentAverage = average / gainedCfu
+                currentProjectedGraduationGrade = currentAverage * 110.0 / 30.0
+            }
             asExpected = Double(passedExams.filter { $0.mark == $0.expectedMark }.count)
             betterThanExpected = Double(passedExams.filter { $0.mark > $0.expectedMark }.count)
             worseThanExpected = Double(passedExams.filter { $0.mark < $0.expectedMark }.count)
